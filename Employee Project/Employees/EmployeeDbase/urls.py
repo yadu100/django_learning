debbc4a8d48b66adc084b642098fa4a36bd1d816
Yadu <ykbs100@gmail.com>
2024-07-18 10:32:47 +0530

    Addition of crud functionalities

--- Employee Project/Employees/EmployeeDbase/urls.py
+++ Employee Project/Employees/EmployeeDbase/urls.py
@@ -5,6 +5,11 @@ from . import views
 
 urlpatterns = [
     path('', views.callHomePage, name="Home"),
-    path('Employee/<str:emp_name>', views.callSingleEmployeePage, name="Employee")
+    path('Employee/<str:emp_name>', views.callSingleEmployeePage, name="Employee"),
+    path('add-employee/',views.callEmployeeAdditionPage,name="add-employee"),
+    path('update-employee/<str:name>', views.callEmployeeUpdateForm, name="update-employee"),
+    path('delete-employee/<str:name>',views.callEmployeeDeleteForm, name='delete-employee'),
+
+
     
 ]
