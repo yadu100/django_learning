87a8011370bf1170de2e16f27f6df011968791ef
Yadu <ykbs100@gmail.com>
2024-07-28 17:20:40 +0530

    added crispy bootstrap to update form

--- Employee Project/Employees/Employees/settings.py
+++ Employee Project/Employees/Employees/settings.py
@@ -40,7 +40,15 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'EmployeeDbase.apps.EmployeedbaseConfig',
+
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
